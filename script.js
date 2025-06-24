c862a07352de4b4480a1858dba1e2f930d420a01
lauramatias2025 <laura.matias12@escola.pr.gov.br>
2025-06-24 08:37:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click',MostraPalavraschave);
+
+function MostraPalavraschave(){
+   alert('fui clicado!')
+}
