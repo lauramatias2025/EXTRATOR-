81a67a6d41ad8613b2c6573ebe720d7ddd753d26
lauramatias2025 <laura.matias12@escola.pr.gov.br>
2025-08-05 08:51:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener('click',MostraPalavraschave);
+botaoMostraPalavras.addEventListener('click',mostraPalavraschave);
 
-function MostraPalavraschave(){
-   alert('fui clicado!')
+function mostraPalavraschave(){
+   const texto = document.querySelector ('#entrada-de-texto').value;
+   const campoResultado = document.querySelector('#resultado-palavrachave');
+   const palavras = texto.split (" ");
+   campoResultado.textContent = palavras.join(", "); 
 }
